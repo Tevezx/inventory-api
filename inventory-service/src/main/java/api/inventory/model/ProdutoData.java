0673a10860b260c823485917a79cc5d8e0c77981
Tevezx <teveztwitch@gmail.com>
2026-06-25 22:52:07 -0300

    feat: adicionando repository com metodos crud do produto

--- inventory-service/src/main/java/api/inventory/model/ProdutoData.java
+++ inventory-service/src/main/java/api/inventory/model/ProdutoData.java
@@ -2,23 +2,26 @@ package api.inventory.model;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import org.springframework.stereotype.Component;
 
 import java.util.ArrayList;
 import java.util.List;
 
+@Component
 @AllArgsConstructor
-@Getter
 public class ProdutoData {
-    private static final List<Produto> produtoList = new ArrayList<>();
+    @Getter
+    private final List<Produto> produtoList = new ArrayList<>();
 
-    static {
-        var mouseLogitech = produtoList.add(Produto.
+    {
+        var mouseLogitech = Produto.
                 builder()
                 .id(1L)
                 .nome("Mouse Gamer Logitech G403 HERO")
                 .descricao("RGB LIGHTSYNC, 6 Botões Programáveis, Ajuste de Peso e Sensor HERO 25K - 910-005631")
                 .preco(190.0)
                 .qtdEstoque(10)
-                .build());
+                .build();
+        produtoList.add(mouseLogitech);
     }
 }
--- inventory-service/src/main/java/api/inventory/repository/ProdutoRepository.java
+++ inventory-service/src/main/java/api/inventory/repository/ProdutoRepository.java
@@ -1,8 +1,46 @@
 package api.inventory.repository;
 
+import api.inventory.model.Produto;
+import api.inventory.model.ProdutoData;
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Repository;
 
-@Repository
-public class ProdutoRepository {
+import java.util.List;
+import java.util.Optional;
 
+@Repository
+@AllArgsConstructor
+public class ProdutoRepository {
+    private final ProdutoData produtoData;
+
+    public List<Produto> findAll() {
+        return produtoData.getProdutoList();
+    }
+
+    public Optional<Produto> findById(Long id) {
+        return produtoData.getProdutoList()
+                .stream()
+                .filter(produto -> produto.getId().equals(id))
+                .findFirst();
+    }
+
+    public List<Produto> listAllName(String nome) {
+        return produtoData.getProdutoList()
+                .stream()
+                .filter(produto -> produto.getNome().equalsIgnoreCase(nome))
+                .toList();
+    }
+
+    public void save(Produto produto) {
+        produtoData.getProdutoList().add(produto);
+    }
+
+    public void deleteById(Long id) {
+        produtoData.getProdutoList().removeIf(produto -> produto.getId().equals(id));
+    }
+
+    public void update(Produto produto) {
+        deleteById(produto.getId());
+        save(produto);
+    }
 }

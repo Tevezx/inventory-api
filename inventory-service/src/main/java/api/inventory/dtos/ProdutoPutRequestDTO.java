a4fd1d7f6471ac4a824cd3b39fc6ea0c90b8333f
Tevezx <teveztwitch@gmail.com>
2026-07-13 18:14:15 -0300

    feat: adicionando bean validation aos produtos request

--- inventory-service/src/main/java/api/inventory/dtos/ProdutoPutRequestDTO.java
+++ inventory-service/src/main/java/api/inventory/dtos/ProdutoPutRequestDTO.java
@@ -1,14 +1,21 @@
 package api.inventory.dtos;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
 public class ProdutoPutRequestDTO {
+    @NotNull(message = "Id obrigatório")
     private Long id;
+    @NotBlank(message = "Nome obrigatório")
     private String nome;
+    @NotBlank(message = "Descrição obrigatória")
     private String descricao;
+    @NotBlank(message = "Preço obrigatório")
     private Double preco;
+    @NotBlank(message = "Quantidade em estoque obrigatório")
     private Integer qtdEstoque;
 }
--- inventory-service/src/main/java/api/inventory/dtos/ProdutoRequestDTO.java
+++ inventory-service/src/main/java/api/inventory/dtos/ProdutoRequestDTO.java
@@ -1,13 +1,18 @@
 package api.inventory.dtos;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
 public class ProdutoRequestDTO {
+    @NotBlank(message = "Nome obrigatório")
     private String nome;
+    @NotBlank(message = "Descrição obrigatória")
     private String descricao;
+    @NotBlank(message = "Preço obrigatório")
     private Double preco;
+    @NotBlank(message = "Quantidade em estoque obrigatório")
     private Integer qtdEstoque;
 }

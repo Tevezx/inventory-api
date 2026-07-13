757267ea740da0e1b7f45897b1b23635afad3420
Tevezx <teveztwitch@gmail.com>
2026-07-13 19:33:08 -0300

    feat: adicionando testes parametrizados com o contexto do bean validation

--- inventory-service/src/main/java/api/inventory/dtos/ProdutoPutRequestDTO.java
+++ inventory-service/src/main/java/api/inventory/dtos/ProdutoPutRequestDTO.java
@@ -3,6 +3,7 @@ package api.inventory.dtos;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.PositiveOrZero;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -15,9 +16,10 @@ public class ProdutoPutRequestDTO {
     private String nome;
     @NotBlank(message = "Descrição obrigatória")
     private String descricao;
-    @NotBlank(message = "Preço obrigatório")
+    @NotNull(message = "Preço obrigatório")
     @Positive(message = "Preço deve ser positivo")
     private Double preco;
-    @NotBlank(message = "Quantidade em estoque obrigatório")
+    @NotNull(message = "Quantidade em estoque obrigatório")
+    @PositiveOrZero(message = "Quantidade em estoque deve ser maior ou igual a zero")
     private Integer qtdEstoque;
 }
--- inventory-service/src/test/java/api/inventory/controller/ProdutoControllerTest.java
+++ inventory-service/src/test/java/api/inventory/controller/ProdutoControllerTest.java
@@ -3,7 +3,11 @@ package api.inventory.controller;
 import api.inventory.model.Produto;
 import api.inventory.model.ProdutoData;
 import api.inventory.repository.ProdutoRepository;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.*;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.mockito.ArgumentMatchers;
 import org.mockito.BDDMockito;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -23,6 +27,7 @@ import java.nio.file.Files;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.stream.Stream;
 
 @WebMvcTest(controllers = ProdutoController.class)
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
@@ -92,9 +97,9 @@ class ProdutoControllerTest {
     }
 
     @Test
-    @DisplayName("GET v1/produtos/90 - Retornando o throw ResponseStatusException, caso o id não seja encontrado")
+    @DisplayName("GET v1/produtos/90 - Retornando o throw NotFoundException, caso o id não seja encontrado")
     @Order(3)
-    void findById_ReturnsThrowResponseStatusException_WhenProdutoIdIsNotFound() throws Exception {
+    void findById_ReturnsThrowNotFoundException_WhenProdutoIdIsNotFound() throws Exception {
         var response = readResourceFile("produto/get-produto-find-by-id-404.json");
 
         BDDMockito.when(produtoData.getProdutoList()).thenReturn(produtoList);
@@ -180,35 +185,8 @@ class ProdutoControllerTest {
     }
 
     @Test
-    @DisplayName("POST v1/produtos - Salvando um produto com nome e preco nulo e retornando throw IlegalArgumentException")
-    @Order(8)
-    void save_ThrowIlegalArgumentException_WhenNameIsNull() throws Exception {
-        var request = readResourceFile("produto/post-produto-request-name-descricao-null-500.json");
-        var response = readResourceFile("produto/post-produto-response-name-descricao-null-500.json");
-
-        var produto = Produto
-                .builder()
-                .id(3L)
-                .nome(null)
-                .descricao(null)
-                .preco(250.0)
-                .qtdEstoque(3)
-                .build();
-
-        BDDMockito.when(repository.save(ArgumentMatchers.any())).thenReturn(produto);
-        mockMvc.perform(MockMvcRequestBuilders
-                        .post("/v1/produtos")
-                        .content(request)
-                        .contentType(MediaType.APPLICATION_JSON_VALUE)
-                )
-                .andDo(MockMvcResultHandlers.print())
-                .andExpect(MockMvcResultMatchers.status().isInternalServerError())
-                .andExpect(MockMvcResultMatchers.content().json(response));
-    }
-
-    @Test
     @DisplayName("DELETE v1/produtos/1 - Deletando um produto pelo id")
-    @Order(9)
+    @Order(8)
     void deleteById_removesProduto_WhenSucessFull() throws Exception {
         var id = 1L;
         BDDMockito.when(produtoData.getProdutoList()).thenReturn(produtoList);
@@ -219,9 +197,9 @@ class ProdutoControllerTest {
     }
 
     @Test
-    @DisplayName("DELETE v1/produtos/90 - Deletando um produto com id inexistente e lançando throw ResponseStatusException")
-    @Order(10)
-    void deleteById_ThrowResponseStatusException_WhenIdIsNotFound() throws Exception {
+    @DisplayName("DELETE v1/produtos/90 - Deletando um produto com id inexistente e lançando throw NotFoundException")
+    @Order(9)
+    void deleteById_ThrowNotFoundException_WhenIdIsNotFound() throws Exception {
         var response = readResourceFile("produto/delete-produto-response-404.json");
 
         BDDMockito.when(produtoData.getProdutoList()).thenReturn(produtoList);
@@ -235,7 +213,7 @@ class ProdutoControllerTest {
 
     @Test
     @DisplayName("PUT v1/produtos - Atualizando produto")
-    @Order(11)
+    @Order(10)
     void update_UpdateProduto_WhenSucessFull() throws Exception {
         var request = readResourceFile("produto/put-produto-request-204.json");
 
@@ -251,8 +229,8 @@ class ProdutoControllerTest {
     }
 
     @Test
-    @DisplayName("PUT v1/produtos - Atualizando produto com id inexistente e lançando throw ResponseStatusException")
-    @Order(12)
+    @DisplayName("PUT v1/produtos - Atualizando produto com id inexistente e lançando throw NotFoundException")
+    @Order(11)
     void update_UpdateProduto_WhenIdIsNotFound() throws Exception {
         var request = readResourceFile("produto/put-produto-request-404.json");
         var response = readResourceFile("produto/put-produto-response-404.json");
@@ -267,6 +245,80 @@ class ProdutoControllerTest {
                 .andDo(MockMvcResultHandlers.print())
                 .andExpect(MockMvcResultMatchers.status().isNotFound())
                 .andExpect(MockMvcResultMatchers.content().json(response));
+    }
+
+    @ParameterizedTest
+    @MethodSource("postBadRequestSource")
+    @DisplayName("POST v1/produtos - Salvando um produto com dados em branco")
+    @Order(12)
+    void save_SavesProduto_WhenEmptyFields(String fileName, List<String> errors) throws Exception {
+        var request = readResourceFile("produto/%s".formatted(fileName));
+
+        var mvcResult = mockMvc.perform(MockMvcRequestBuilders
+                        .post("/v1/produtos")
+                        .content(request)
+                        .contentType(MediaType.APPLICATION_JSON_VALUE)
+                )
+                .andDo(MockMvcResultHandlers.print())
+                .andExpect(MockMvcResultMatchers.status().isBadRequest())
+                .andReturn();
+
+        Exception resolvedException = mvcResult.getResolvedException();
+        Assertions.assertThat(resolvedException).isNotNull();
+
+        Assertions.assertThat(resolvedException.getMessage()).contains(errors);
+    }
+
+    @ParameterizedTest
+    @MethodSource("putBadRequestSource")
+    @DisplayName("PUT v1/produtos - Atualizando produto com id nulo e preços inválidos")
+    @Order(13)
+    void update_UpdateProduto_WhenIdIsNullAndEmptyFields(String fileName, List<String> errors) throws Exception {
+        var request = readResourceFile("produto/%s".formatted(fileName));
+
+        var mvcResult = mockMvc.perform(MockMvcRequestBuilders
+                        .put("/v1/produtos")
+                        .content(request)
+                        .contentType(MediaType.APPLICATION_JSON_VALUE)
+                )
+                .andDo(MockMvcResultHandlers.print())
+                .andExpect(MockMvcResultMatchers.status().isBadRequest())
+                .andReturn();
+    }
+
+    private static Stream<Arguments> postBadRequestSource() {
+        return Stream.of(
+                Arguments.of("post-produto-request-user-blank-fields-400.json", allRequiredErrors()),
+                Arguments.of("post-produto-request-user-negative-fields-400.json", priceAndQtdStockPositive())
+        );
+    }
+
+    private static Stream<Arguments> putBadRequestSource() {
+        return Stream.of(
+                Arguments.of("put-request-user-blank-fields-400.json", allRequiredErrors()),
+                Arguments.of("put-request-user-null-id-400.json", idNotNull()),
+                Arguments.of("put-request-user-price-stock-negative-400.json", priceAndQtdStockPositive())
+        );
+    }
+
+    private static List<String> allRequiredErrors() {
+        var nomeRequired = "Nome obrigatório";
+        var descricaoRequired = "Descrição obrigatória";
+        var precoRequired = "Preço obrigatório";
+        var qtdEstoqueRequired = "Quantidade em estoque obrigatório";
+
+        return List.of(nomeRequired, descricaoRequired, precoRequired, qtdEstoqueRequired);
+    }
+
+    private static List<String> priceAndQtdStockPositive() {
+        var precoPositive = "Preço deve ser positivo";
+        var qtdEstoquePositive = "Quantidade em estoque deve ser maior ou igual a zero";
+        return List.of(precoPositive, qtdEstoquePositive);
+    }
+
+    private static List<String> idNotNull() {
+        var idNotNull = "Id obrigatório";
+        return List.of(idNotNull);
     }
 
     private String readResourceFile(String fileName) throws IOException {
